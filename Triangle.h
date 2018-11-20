5b69c5212629c499cd4fe7792ba7d6fe31fc2cea
maxorneholm <maxorneholm@hotmail.com>
2018-11-20 23:08:04 +0100

    Updated Triangle

--- Triangle.h
+++ Triangle.h
@@ -1,13 +1,12 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
-
 #include "Shape.h"
 
 class Triangle : public Shape{
 protected:
     float circum;
-    float distanceAtoB, distanceAtoC, distanceBtoC;
-    float hypo, base, height, sumX, sumY;
+    float distanceAtoB = 0, distanceAtoC = 0, distanceBtoC = 0;
+    float hypo = 0, base = 0, height = 0, sumX = 0, sumY = 0;
 public:
     void getType();
     void getArea(float*, float*);
