38e2344ffe6d01c5587d91736e0d6a0657bde30f
maxorneholm <maxorneholm@hotmail.com>
2018-11-20 23:08:17 +0100

    Updated main

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ int main(){
     float *coords = new float[*coordCounter];
     float *xCoords = new float[*coordCounter/2];
     float *yCoords = new float[*coordCounter/2];
+    float *kValue = new float[(*coordCounter/2)-1]; 
+
 
     file.close();
     file.open("shape.txt");
@@ -58,13 +60,61 @@ int main(){
 
     std::cout << "\n\n";
 
+   for(int i = 0; i < (*coordCounter/2)-1; i++){
+
+        float yV = (yCoords[i+1] - yCoords[i]);
+        float xV = (xCoords[i+1] - xCoords[i]);
+        float kV = (yV/xV);
+        kValue[i] = kV;
+    }
+
+    bool isLine = false;
+    bool isPolygon = false;
+
+    for(int i=0; i < (*coordCounter/2)-1; i++){
+        if(kValue[0] == kValue[i]){
+            isLine = true;
+        }else{
+            isLine = false;
+            isPolygon = true;
+        }
+    }
+
+    if(isLine == true){
+        Line line;
+        line.getType();
+        line.getArea(xCoords, yCoords);
+    }
+
+    if(isPolygon == true && *coordCounter/2 == 3){
+        Triangle triangle;
+        triangle.getType();
+        triangle.getArea(xCoords, yCoords);
+        triangle.circumference();
+        triangle.position(xCoords, yCoords);
+    }
+    if(isPolygon == true && *coordCounter/2 > 3){
+        std::cout << "\nPoly\n";
+    }
+
     if(*coordCounter/2 == 1){
-        Point point;
+        Point point; 
         point.getType();
         point.getArea(xCoords, yCoords);
         point.position(xCoords, yCoords);
     }
+    /*if(*coordCounter/2 == 3){
+        Triangle triangle;
+        triangle.getType();
+        triangle.getArea(xCoords, yCoords);
+        triangle.circumference();
+        triangle.position(xCoords, yCoords);
+    }
 
+/*
+   // Point
+
+    // Triangle
     if(*coordCounter/2 == 3 && xCoords[0] > xCoords[1] || xCoords[1] > xCoords[0] || xCoords[0] > xCoords[2] || xCoords[2] > xCoords[0]){
         Triangle triangle;
         triangle.getType();
@@ -73,14 +123,21 @@ int main(){
         triangle.position(xCoords, yCoords);
     }
     
-    if(*coordCounter/2 == 3 && xCoords[0] == xCoords[1] == xCoords[2]){
-        Line line;
-        line.getType();
-        line.getArea(xCoords, yCoords);
-
+    // Line
+    if(*coordCounter/2 > 3){
+        std::cout << "Polygon.";
     }
-
-
+*/
+    delete[] coords;
+    delete[] xCoords;
+    delete[] yCoords;
+    delete coordCounter;
+    coords = NULL;
+    xCoords = NULL;
+    yCoords = NULL;
+    coordCounter = NULL;
+    
+    std::cout << '\n';
     getchar();
     return 0;
 }
