8067265357a2c172ee1b1ee7c107c2a544b1e419
maxorneholm <maxorneholm@hotmail.com>
2018-11-20 17:53:02 +0100

    Update

---
+++ Line.cpp
@@ -0,0 +1,17 @@
+#include "Line.h"
+#include <iostream>
+
+void Line::getType(){
+    type = "Line";
+    std::cout << "Type: " << type << std::endl;
+}
+void Line::position(float* xCoords, float* yCoords){
+    std::cout << "\nCenter coordinates: " << "(" << xCoords[0] << ", " << yCoords[0] << ")" << std::endl;
+}
+
+void Line::getArea(float* xCoords, float* yCoords){
+    area = -1;
+    std::cout << "Area: " << area << " l.e";
+}
+
+void Line::circumference(){}
