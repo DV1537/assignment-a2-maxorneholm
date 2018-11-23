59ae646a15bc8244a8b31cfa1bc7c9b89ceb85c5
maxorneholm <maxorneholm@hotmail.com>
2018-11-23 15:40:06 +0100

    Update Polygon

--- Polygon.cpp
+++ Polygon.cpp
@@ -33,9 +33,57 @@ void Polygon::getArea(float* xCoords, float* yCoords, int* coordCounter){
     total = abs(total);
     total = total/2;
 
-    std::cout << "area: " << total << " l.e";
+    std::cout << "area: " << total << ".";
 }
 
 void Polygon::getArea(float*, float*){}
 void Polygon::circumference(){}
-void Polygon::position(float*, float*){}
+
+void Polygon::position(float* xCoords, float* yCoords, int* coordCounter){
+    int i=0;
+
+    for (i=0; i < (*coordCounter/2)-1; ++i)
+    {
+        xCoord0 = xCoords[i];
+        yCoord0 = yCoords[i];
+        xCoord1 = xCoords[i+1];
+        yCoord1 = yCoords[i+1];
+        a = xCoord0*yCoord1 - xCoord1*yCoord0;
+        currentArea += a;
+        centroidX += (xCoord0 + xCoord1)*a;
+        centroidY += (yCoord0 + yCoord1)*a;
+    }
+
+    xCoord0 = xCoords[i];
+    yCoord0 = yCoords[i];
+    xCoord1 = xCoords[0];
+    yCoord1 = yCoords[0];
+    a = xCoord0*yCoord1 - xCoord1*yCoord0;
+    currentArea += a;
+    centroidX += (xCoord0 + xCoord1)*a;
+    centroidY += (yCoord0 + yCoord1)*a;
+
+    currentArea *= 0.5;
+    centroidX /= (6.0*currentArea);
+    centroidY /= (6.0*currentArea);
+
+    std::cout << "\n" << "Position: (" << centroidX << ", " << centroidY << ")\n";
+   
+   
+   
+   
+   /* float cX = 0;
+    float cY = 0;
+    for(int i = 0; i < (*coordCounter/2)-1; i++){
+	//ta fram x cord
+	cX += (xCoords[i] + xCoords[i+1]) * (xCoords[i] * yCoords[i+1] - xCoords[i+1] + yCoords[i]);
+	//ta fram y cord
+	cY += (yCoords[i] + yCoords[i+1]) * (xCoords[i] * yCoords[i+1] - xCoords[i+1] + yCoords[i]);
+
+    cX += (x0 + x1)*a;
+    cY += (y0 + y1)*a;
+    std::cout << "\nCenter coordinates: " << "(" << (0.01515151515*cX) << ", " << (0.01515151515*cY) << ")"; 
+    }
+*/
+}
+void Polygon::position(float* xCoords, float* yCoords){}
--- Polygon.h
+++ Polygon.h
@@ -4,11 +4,14 @@
 
 class Polygon : public Shape{
 private:
-
+//floats 
+float currentArea = 0, centroidX = 0, centroidY = 0;
+float xCoord0 = 0, yCoord0 = 0, xCoord1 = 0, yCoord1 = 0, a = 0;
 public:
     void getType();
     void circumference();
     void getArea(float*, float*, int*);
+    void position(float*, float*, int*);
     void getArea(float*, float*);
     void position(float*, float*);
     virtual ~Polygon() = default;
