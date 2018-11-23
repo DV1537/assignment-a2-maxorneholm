9389565f5b54149a19804994785fff0b8181aa62
maxorneholm <maxorneholm@hotmail.com>
2018-11-23 15:40:39 +0100

    Update Triangle

--- Triangle.cpp
+++ Triangle.cpp
@@ -5,27 +5,22 @@
 
 void Triangle::getType(){
     type = "Triangle";
-    std::cout << "Type: " << type;
+    std::cout << "\nType: " << type;
 }
 
 void Triangle::getArea(float* xCoords, float* yCoords){
-    // Get the length from A -> B
+    // Get the length A -> B
     float xAtoB = pow(xCoords[1]-xCoords[0],2);
     float yAtoB = pow(yCoords[1]-yCoords[0],2);
     distanceAtoB = sqrt(xAtoB+yAtoB);
-    // Get the length from A -> C
+    // Get the length A -> C
     float xAtoC = pow(xCoords[2]-xCoords[0],2);
     float yAtoC = pow(yCoords[2]-yCoords[0],2);
     distanceAtoC = sqrt(xAtoC+yAtoC); 
-    // Get the length from B -> C
+    // Get the length B -> C
     float xBtoC = pow(xCoords[2]-xCoords[1],2);
     float yBtoC = pow(yCoords[2]-yCoords[1],2);
     distanceBtoC = sqrt(xBtoC+yBtoC);
-/*
-    std::cout << '\n' << "A to B: " << distanceAtoB;
-    std::cout << '\n' << "A to C: " << distanceAtoC;
-    std::cout << '\n' << "B to C: " << distanceBtoC;
-*/
 
     if(distanceAtoB > distanceAtoC && distanceAtoB > distanceBtoC){
         hypo = distanceAtoB;
@@ -46,9 +41,7 @@ void Triangle::getArea(float* xCoords, float* yCoords){
     circumFormula = (distanceAtoB + distanceAtoC + distanceBtoC)/2;
     area = sqrt(circumFormula*(circumFormula-distanceAtoB)*(circumFormula-distanceBtoC)*(circumFormula-distanceAtoC));
 
-   // area = (base*height)/2.0f;
-    std::cout << '\n' << "Area: " << area << " l.e";
-    std::cout << '\n' << distanceAtoB << " " << distanceAtoC << " " << distanceBtoC;
+    std::cout << '\n' << "Area: " << area;
 }
 
 void Triangle::circumference(){
--- Triangle.h
+++ Triangle.h
@@ -6,7 +6,9 @@ class Triangle : public Shape{
 protected:
     float circum = 0, circumFormula = 0;
     float distanceAtoB = 0, distanceAtoC = 0, distanceBtoC = 0;
+    float distance = 0;
     float hypo = 0, base = 0, height = 0, sumX = 0, sumY = 0;
+    float xAtoB, yAtoB, xAtoC, yAtoC, xBtoC, yBtoc = 0;
 public:
     void getType();
     void getArea(float*, float*);
